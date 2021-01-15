9c2feb6c88f10a30d2e30f6ea5b5f5d87fa754c6
jjjggg111 <thisisjungjiwon@gmail.com>
2021-01-15 18:20:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import Banner from './Banner';
 import Nav from './Nav';
 
 function App() {
-	return (
-		<div className="app">
-			<Nav />
-			<Banner />
-			<Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
-			<Row title="Trending" fetchUrl={requests.fetchTrending} />
-			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-		</div>
-	);
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
 
 export default App;
